b1c93344cd191356d98ed40a9e9ba18013ca8c06
RudraaGahlot <rudragahlot111@gmail.com>
2025-03-10 11:40:04 +0530

    added landing page and Authentication

--- components/hero.jsx
+++ components/hero.jsx
@@ -27,7 +27,7 @@ const HeroSection = () => {
   }, []);
 
   return (
-    <section className="w-full pt-36 md:pt-48 pb-10 bg-black text-white">
+    <section className="w-full pt-36 md:pt-48 pb-10 ">
       <div className="space-y-6 text-center">
         <div className="space-y-6 mx-auto">
           <h1 className="text-5xl font-bold md:text-6xl lg:text-7xl xl:text-8xl gradient-title animate-gradient">
@@ -40,22 +40,17 @@ const HeroSection = () => {
             AI-powered tools for job success.
           </p>
         </div>
-        <div className="flex justify-center space-x-4">
+        <div className="flex justify-center space-x-4 cursor-pointer">
           <Link href="/dashboard">
-            <Button size="lg" className="px-8">
+            <Button size="lg" className="px-8 ">
               Get Started
-            </Button>
-          </Link>
-          <Link href="https://www.youtube.com/roadsidecoder">
-            <Button size="lg" variant="outline" className="px-8">
-              Watch Demo
             </Button>
           </Link>
         </div>
         <div className="hero-image-wrapper mt-5 md:mt-0">
           <div ref={imageRef} className="hero-image">
             <Image
-              src="/banner.jpeg"
+              src="/banner3.jpeg"
               width={1280}
               height={720}
               alt="Dashboard Preview"
